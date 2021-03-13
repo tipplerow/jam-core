8e102928e54e9d9d5fb00417966763beead7acef
tipplerow <jsshaff@berkeley.edu>
2021-03-13 15:17:47 -0800

    Process unit tests

---
+++ src/test/java/com/tipplerow/jam/process/JamProcessTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 Scott Shaffer - All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tipplerow.jam.process;
+
+import java.util.List;
+
+import com.tipplerow.jam.junit.JamTestBase;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class JamProcessTest extends JamTestBase {
+    @Test public void testRun() {
+        JamProcess process = JamProcess.run("ls");
+
+        assertTrue(process.success());
+        assertTrue(process.stdout().contains("README.md"));
+        assertTrue(process.stderr().isEmpty());
+
+        process = JamProcess.run("cat", "fooxyz");
+        
+        assertFalse(process.success());
+        assertTrue(process.stdout().isEmpty());
+        assertEquals(List.of("cat: fooxyz: No such file or directory"), process.stderr());
+        
+        process = JamProcess.run("cat", "LICENSE");
+
+        assertEquals(201, process.stdout().size());
+    }
+
+    @Test public void testSetEnv() {
+        JamProcess process = JamProcess.create("env");
+
+        assertFalse(process.executed());
+
+        process.run();
+
+        assertTrue(process.executed());
+        assertFalse(process.stdout().contains("MYTESTVAR=foo"));
+
+        process.setenv("MYTESTVAR", "foo");
+        process.run();
+
+        assertTrue(process.executed());
+        assertTrue(process.stdout().contains("MYTESTVAR=foo"));
+    }
+}
---
+++ src/test/java/com/tipplerow/jam/process/ProcessRunnerTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 Scott Shaffer - All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tipplerow.jam.process;
+
+import com.tipplerow.jam.junit.JamTestBase;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ProcessRunnerTest extends JamTestBase {
+    @Test public void testRun() {
+        assertTrue(ProcessRunner.run("ls").contains("README.md"));
+        assertEquals(201, ProcessRunner.run("cat", "LICENSE").size());
+    }
+}
